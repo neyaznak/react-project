cd2b8a6c06997fac28e44c6134ad1e6498fedcd9
MohammadNAKnw <neyaznak@gmail.com>
2023-12-02 15:14:28 +0530

    foundation03

--- App.js
+++ App.js
@@ -1,50 +1,98 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+//React.createElement ==> Object ==> HTMLElement
 
+//JSX(transpiled before it reaches the JS) - Parcel - Babel
 
+//JSX => React.createElement => ReactElement -JS Object +> HTMLElement(render)
+//React Element
 
 /**
- * create this html using react only
- * 
- * <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h1>I'm h2 tag</h1>
-    </div>
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h1>I'm h2 tag</h1>
-    </div>
-</div>
- * 
+ *
+ * using React.createElement only(no JSX, no Functional Component)
+ *
  */
 
-/***
- * usinh js 
- *  "div",
-    { id: "parent"},React.createElement("div", {id: "child"},
-    React.createElement("h1", {}, "I'm an h1 tag"))
- * 
+// const heading = React.createElement(
+//     "div",
+//     {className: "title"},
+//     React.createElement("h1",{},"This is H1 normal"),
+//     React.createElement("h2",{},"This is H2 normal"),
+//     React.createElement("h3",{},"This is H3 normal")
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+/**
+ *
+ * using JSX
  */
 
+// const jsxHeading = (
+//     <div className="title">
+//         <h1>This is H1 JSX</h1>
+//         <h2>This is H2 JSX</h2>
+//         <h3>This is H3 JSX</h3>
+//     </div>
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
 
-//using react
+/**
+ * using functional component
+ */
 
+// const FnCompHeading = () => (
+//     <div className="title">
+//         <h1>This is H1 functional</h1>
+//         <h2>This is H2 functional</h2>
+//         <h3>This is H3 functional</h3>
+//     </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<FnCompHeading />);
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
+import logo from './images/logo.png';
+import userIcon from './images/userIcon.png';
 
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-]);
-
-console.log(parent); //logs an Object (a react element)
+const Header = () => {
+  return (
+  <div className="header">
+    <div className="logo">
+      <img src={logo} alt="Logo" />
+    </div>
+    <div className="search-bar">
+      <input type="text" placeholder="Search..." />
+    </div>
+    <div className="user-icon">
+      <img src={userIcon} alt="User Icon" />
+    </div>
+  </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
 
-root.render(parent);
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hi React using JSX!!!
+  </h1>
+);
+
+//React Functional Component
+//Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">Hi React Functional Component</h1>
+  </div>
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+// root.render(<HeadingComponent/>);
